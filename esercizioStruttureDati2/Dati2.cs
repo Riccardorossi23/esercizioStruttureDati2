03a44a6020e975624f833e7019c3c44199cb874c
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-01-27 09:43:18 +0100

    Add project files.

---
+++ esercizioStruttureDati2/Dati2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace esercizioStruttureDati2
+{
+    class Dati2
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> numeri = new HashSet<int>();
+            using (StreamReader sr = new StreamReader("dati2.txt", Encoding.UTF8))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    int n = int.Parse(line);
+                    numeri.Add(n);
+                }
+            }
+            using (StreamWriter sw = new StreamWriter("file2dati_fake.txt", false, Encoding.UTF8))
+            {
+                foreach (int s in numeri)
+                {
+                    sw.WriteLine(s);
+                }
+            }
+        }
+
+    }
+}
